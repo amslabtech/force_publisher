2181173de38336d0b46de2ddcf1a5683bf670a30
ykuroda513 <ykuroda@meiji.ac.jp>
2019-12-17 18:11:18 +0900

    bag fix

--- include/force_structure.hpp
+++ include/force_structure.hpp
@@ -13,20 +13,20 @@ struct ForceDataStructure {
 	int32_t id;
 	int32_t sec;
 	int32_t usec;
-	float  gain [6];	// forces
-	float  force[6];	// forces
+	float  gain [8];	// forces
+	float  force[8];	// forces
 
 	void print() {
 		std::cout
-		<< std::setw( 6) << id
+		<< std::setw(06) << std::dec << id
 //		<< std::setw(12) << ts.tv_sec
 //		<< std::setw(10) << ts.tv_usec
-		<< std::setw(12) << force[0]
-		<< std::setw(12) << force[1]
-		<< std::setw(12) << force[2]
-		<< std::setw(12) << force[3]
-		<< std::setw(12) << force[4]
-		<< std::setw(12) << force[5]
+		<< std::setw(16) << force[0]
+		<< std::setw(16) << force[1]
+		<< std::setw(16) << force[2]
+		<< std::setw(16) << force[3]
+		<< std::setw(16) << force[4]
+		<< std::setw(16) << force[5]
 		<< std::endl;
 	}
 
--- src/force_publisher.cpp
+++ src/force_publisher.cpp
@@ -13,24 +13,42 @@
 #include "force_structure.hpp"	// force data types
 #include "RTIMULib.h"			// force library
 using namespace force;
+using namespace std;
 
 std::mutex mtx;
-volatile int renew_flag = 0;
 ForceDataStructure forcedata;
+volatile uint8_t renew_flag = 0;
 
 
 //Declare any event handlers here. These will be called every time the associated event occurs.
-static void CCONV onVoltageRatioChange(
-	PhidgetVoltageRatioInputHandle ch, void * ctx, double voltageRatio) {
+static void CCONV
+handler0(PhidgetVoltageRatioInputHandle ch, void * ctx, double voltageRatio)
+{
 	int channel;
 
+	mtx.lock();
 	//Getting the channel number to distinguish between Phidgets
 	Phidget_getChannel((PhidgetHandle)ch, &channel);
 //	printf("VoltageRatio [%d]: %lf\n", channel, voltageRatio);
 
-	mtx.lock();
 	forcedata.force[channel] = voltageRatio;
-	renew_flag = 1<<channel;
+	renew_flag |= 0x01<<channel;
+	mtx.unlock();
+}
+
+//Declare any event handlers here. These will be called every time the associated event occurs.
+static void CCONV
+handler1(PhidgetVoltageRatioInputHandle ch, void * ctx, double voltageRatio)
+{
+	int channel;
+
+	mtx.lock();
+	//Getting the channel number to distinguish between Phidgets
+	Phidget_getChannel((PhidgetHandle)ch, &channel);
+//	printf("VoltageRatio [%d]: %lf\n", channel, voltageRatio);
+
+	forcedata.force[channel+4] = voltageRatio;
+	renew_flag |= 0x10<<channel;
 	mtx.unlock();
 }
 
@@ -44,15 +62,13 @@ int main()
 	const char* username = "force";
 	struct timeval ts;
 
-
 	//Declare your Phidget channels and other variables
-	PhidgetVoltageRatioInputHandle fore_left;
 	PhidgetVoltageRatioInputHandle fore_right;
+	PhidgetVoltageRatioInputHandle fore_left;
 	PhidgetVoltageRatioInputHandle center_left;
 	PhidgetVoltageRatioInputHandle center_right;
-	PhidgetVoltageRatioInputHandle rear_left;
 	PhidgetVoltageRatioInputHandle rear_right;
-
+	PhidgetVoltageRatioInputHandle rear_left;
 
 	//Create your Phidget channels
 	PhidgetVoltageRatioInput_create(&fore_left);
@@ -63,57 +79,60 @@ int main()
 	PhidgetVoltageRatioInput_create(&rear_right);
 
 	//Set addressing parameters to specify which channel to open (if any)
-	Phidget_setDeviceSerialNumber((PhidgetHandle)fore_left,   123456);
-	Phidget_setDeviceSerialNumber((PhidgetHandle)fore_right,  123456);
-	Phidget_setDeviceSerialNumber((PhidgetHandle)center_left, 123456);
-	Phidget_setDeviceSerialNumber((PhidgetHandle)center_right,123456);
-	Phidget_setDeviceSerialNumber((PhidgetHandle)rear_left,   123456);
-	Phidget_setDeviceSerialNumber((PhidgetHandle)rear_right,  123456);
+	Phidget_setDeviceSerialNumber((PhidgetHandle)fore_right,  589194);
+	Phidget_setChannel((PhidgetHandle)fore_right,  0);
+	Phidget_setDeviceSerialNumber((PhidgetHandle)fore_left,   589194);
+	Phidget_setChannel((PhidgetHandle)fore_left,   1);
+	Phidget_setDeviceSerialNumber((PhidgetHandle)center_left, 589194);
+	Phidget_setChannel((PhidgetHandle)center_left, 2);
+	Phidget_setDeviceSerialNumber((PhidgetHandle)center_right,589194);
+	Phidget_setChannel((PhidgetHandle)center_right,3);
+
+	Phidget_setDeviceSerialNumber((PhidgetHandle)rear_left,   583340);
+	Phidget_setChannel((PhidgetHandle)rear_left,   0);
+	Phidget_setDeviceSerialNumber((PhidgetHandle)rear_right,  583340);
+	Phidget_setChannel((PhidgetHandle)rear_right,  3);
 	
 	//Assign any event handlers you need before calling open so that no events are missed.
-	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(   fore_left, onVoltageRatioChange, NULL);
-	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(  fore_right, onVoltageRatioChange, NULL);
-	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler( center_left, onVoltageRatioChange, NULL);
-	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(center_right, onVoltageRatioChange, NULL);
-	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(   rear_left, onVoltageRatioChange, NULL);
-	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(  rear_right, onVoltageRatioChange, NULL);
+	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(   fore_left, handler0, NULL);
+	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(  fore_right, handler0, NULL);
+	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler( center_left, handler0, NULL);
+	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(center_right, handler0, NULL);
+	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(   rear_left, handler1, NULL);
+	PhidgetVoltageRatioInput_setOnVoltageRatioChangeHandler(  rear_right, handler1, NULL);
 
 	//Open your Phidgets and wait for attachment
-	Phidget_openWaitForAttachment((PhidgetHandle)fore_left,   5000);
-	Phidget_openWaitForAttachment((PhidgetHandle)fore_right,  5000);
-	Phidget_openWaitForAttachment((PhidgetHandle)center_left, 5000);
-	Phidget_openWaitForAttachment((PhidgetHandle)center_right,5000);
-	Phidget_openWaitForAttachment((PhidgetHandle)rear_left,   5000);
-	Phidget_openWaitForAttachment((PhidgetHandle)rear_right,  5000);
-
-
-
+	Phidget_openWaitForAttachment((PhidgetHandle)fore_left,   7);
+	Phidget_openWaitForAttachment((PhidgetHandle)fore_right,  7);
+	Phidget_openWaitForAttachment((PhidgetHandle)center_left, 7);
+	Phidget_openWaitForAttachment((PhidgetHandle)center_right,7);
+	Phidget_openWaitForAttachment((PhidgetHandle)rear_left,   7);
+	Phidget_openWaitForAttachment((PhidgetHandle)rear_right,  7);
 
 
 	force_publisher.set_username_password(username,password);
 	force_publisher.connect(ip_addr);
 	force_publisher.subscribe(topic);
 
-	std::cout << "\ndatasize = " << sizeof(ForceDataStructure) << " bytes\n";
+	cout << "\ndatasize = " << sizeof(ForceDataStructure) << " bytes\n";
 
-	int i=0;
-	while(1) {
+	for(int i=0;;) {
 
-		usleep(1000);
+		usleep(1*1000);
 
-		mtx.lock();
-		if(renew_flag==0x3F) {
+		if(renew_flag>=0x1F) {
 			gettimeofday(&ts, NULL);
-			forcedata.id  = i;
+			forcedata.id  = i++;
 			forcedata.sec = ts.tv_sec;
 			forcedata.usec= ts.tv_usec;
 
+cout << "flag:" << hex << +renew_flag ;
+forcedata.print();
+			mtx.lock();
 			force_publisher.publish(topic,(void*)&forcedata,sizeof(forcedata));
-			//forcedata.print();
 			renew_flag = 0;
-			i++;
+			mtx.unlock();
 		}
-		mtx.unlock();
 
 	}
 	sleep(1);
